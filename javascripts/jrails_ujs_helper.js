9ae2c49d507ac2ea44e89f8680a8c665f1dcf0ff
Matt Swasey <mig@pggbee.com>
2008-11-07 17:19:05 -0500

    started the readme

--- javascripts/jrails_ujs_helper.js
+++ javascripts/jrails_ujs_helper.js
@@ -1,4 +1,18 @@
+// Grab the authenticity token from the variables created by yield_authenticity_token
+var auth_token_params = encodeURIComponent(window._auth_token_name)
+			                    + "=" + encodeURIComponent(window._auth_token);
+
+// Ajaxify links and form elements with ujs class
 $(document).ready(function() {
 	$('a.ujs').click(function() {return false;});
-	$('form.ujs').submit(function() {return false;});
+	$('form.ujs').submit(function() {
+		$.ajax({
+			data: $.param($(this).serializeArray()) + auth_token_params, 
+			dataType: 'script', 
+			type: 'post', 
+			url: this.attr('action')
+		});
+		this.reset();		
+		return false;
+	});
 });
