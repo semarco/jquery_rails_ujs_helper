8df0c82331128999cf5e45f1a3e9aac13cf64ee6
Matt Swasey <mig@pggbee.com>
2008-11-07 16:46:42 -0500

    added auth token helpers

--- lib/jrails_ujs_helper.rb
+++ lib/jrails_ujs_helper.rb
@@ -27,4 +27,18 @@ module JrailsUjsHelper
 
     form_tag(url_for_options = {}, options, *parameters_for_url, &block) 
   end
+  
+  def yield_authenticity_token
+    if protect_against_forgery?
+      <<-JAVASCRIPT
+<script type='text/javascript'>
+  //<![CDATA[
+    window._auth_token_name = "#{request_forgery_protection_token}";
+    window._auth_token = "#{form_authenticity_token}";
+  //]]>
+</script>
+      JAVASCRIPT
+    end
+  end
+  
 end
